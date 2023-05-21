9c34de0f89c55a153f0ba241a7023d188d1ec0ac
Muhammad Emran Hossain <emranhss@gmail.com>
2023-05-21 12:58:04 +0600

    add

---
+++ practice/script.js
@@ -0,0 +1,25 @@
+let radius=5;
+const pi=3.1416;
+
+let areaOfCircle=2*(pi*radius*radius);
+
+// console.log(areaOfCircle);
+
+// < > == != =< => 
+let x=10;
+let y=7;
+
+console.log(x<y);
+console.log(x>y);
+console.log(x=y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x!=y);
+
+
+// Logical operator
+// and(&&) or(||) not (!)
+
+// !(username == "java" && password=="1234565" && (gender== "male"  || gender =="female")&& country =="BD")
+
+
---
+++ practice/test-If_statement.js
@@ -0,0 +1,49 @@
+// 0-18 Cool , 19-27 normal , 28 to unlimited abnormal
+// let weather=-100;
+
+// if(weather<0){
+// console.log("Invalid weather");
+// }
+// else if(weather<=18){
+//     console.log("Cool weather");
+// }
+// else if(weather<=27){
+//     console.log("normal weather");
+// }
+// else
+// {
+//     console.log("abnormal weather");
+// }
+
+
+
+// for(let i=1; i<=50; i++){
+     
+//     if(i%3==0 && i%5==0){
+//         console.log(i); 
+//     }
+//     else{
+
+//     }
+
+// }
+
+
+// 5!
+// 5*4!
+// 5*4*3!
+// 5*4*3*2*1
+
+let input=1;
+let result=1;
+
+for(let i=input; i>1; i--){
+
+    result=result*i;
+    // result *=i;   
+
+}
+console.log(result);
+
+
+
