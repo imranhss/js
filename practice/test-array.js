cf1b61be9680d73447a571c5f65e1f8707c01714
Muhammad Emran Hossain <emranhss@gmail.com>
2023-05-23 12:56:55 +0600

    add

---
+++ practice/test-array.js
@@ -0,0 +1,16 @@
+let names=[10];
+
+names[0]="rajib";
+names[1]="Java";
+names[2]="Python";
+names[3]="rajib";
+names[4]="Java";
+names[5]="Python";
+names[6]="rajib";
+names[7]="Java";
+names[8]="Python";
+names[9]="Python";
+names[10]="Python";
+
+console.log(names[7]);
+
